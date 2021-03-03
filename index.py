eb7957d93ee185e658bfc30f8ac246e3689023d3
Melany <77691160+piemusician@users.noreply.github.com>
2021-03-03 09:50:14 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
+from flask import Flask, url_for, render_newfolder, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_newfolder('home.html')
+
+@app.route("/response")
+def render_response():
+    color = request.args['color'] 
+    #The request object stores information about the request sent to the server.
+    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
+    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
+    if color == 'pink':
+        reply = "That's my favorite color, too!"
+    else:
+        reply = "My favorite color is pink."
+    return render_template('response.html', response = reply)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
 
