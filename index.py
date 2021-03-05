8d38911e6a1f2b4f8b22520e49935cf1123ca812
Melany Nunez <melanynunez@Melanys-MacBook-Pro.local>
2021-03-05 09:36:09 -0800

    I've created responses for 2010 & Pop

--- index.py
+++ index.py
@@ -8,14 +8,19 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    era = request.args['era'] 
+    era = request.args['era']
+    genre = request.args['genre'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if era == '2010':
-        reply = "That era brings me good memories!"
+        reply = "That era brings me good memories! I listened to pop."
+        if genre == 'pop':
+        		reply = "I appreciate Ariana Grande's Yours Truly Album"
     else:
-        reply = "My favorite era is 2010."
+        	reply = "My favorite era is 2010 and genre is pop. I really like Ariana Grande's Yours Truly Album."
+        	
+        	
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
